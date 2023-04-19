5a01ef5f0bc08b5da2ac8c719099f2419a67c2ef
YongHwanLee97 <dldyd1101@gmail.com>
2023-04-19 17:28:15 +0900

    4월19일

---
+++ Day02/DayWpfApp/wp05_bikeshop/Logics/Car.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace wp05_bikeshop.Logics
+{
+    internal class Car
+    {
+        public string Names { get; set; }    // Auto Property
+        public double Speed { get; set; }
+        public Color Colors { get; set; }
+        public Human driver { get; set; }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # pknu-wpf-2023
 부경대 IoT 시스템개발자 WPF(데스크탑 앱 개발) 리포지토리
+
+Windows Presentation Foundation
+
+## 1일차
+- WPF 기초
+	- WPF 개요 및 윈폼과의 차이점
+	- 기본컨트롤
+	
+## 2일차
+- WPF 기초
+	- 기본컨트롤
+	- 레이아웃 Grid...
+	- BikeShop 시작
+	- WPF만의 개발방식
+	- 데이터바인딩
+	
+## 3일차
+-	WPF기초
+	- 데이터바인딩계속
